2bbc0740d69c4aa8ab11925418cc284d61ad5952
ToprakOzgur <zgrtprk@gmail.com>
2020-05-06 09:32:46 +0300

    smooth scrool added

--- js/app.js
+++ js/app.js
@@ -22,3 +22,40 @@ window.addEventListener("scroll", () => {
         navbar.classList.remove("fixed");
     }
 });
+
+// scroll smooth
+
+const scrollLinks = document.querySelectorAll(".scroll-link");
+
+scrollLinks.forEach((link) => {
+    link.addEventListener("click", e => {
+        e.preventDefault();
+
+        links.classList.remove("show-links");
+
+        const id = e.target.getAttribute("href").slice(1);
+        const element = document.getElementById(id);
+
+        let position;
+        if (navbar.classList.contains("fixed")) {
+            position = element.offsetTop - 62;
+        } else {
+            position = element.offsetTop - 124;
+        }
+
+        if (window.innerWidth < 992) {
+            if (navbar.classList.contains("fixed")) {
+                position = element.offsetTop - 62;
+            } else {
+
+                position = element.offsetTop - 332 - 62;
+            }
+        }
+
+        window.scrollTo({
+            left: 0,
+            top: position,
+            behavior: "smooth"
+        });
+    });
+});
